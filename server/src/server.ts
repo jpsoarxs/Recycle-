87faafc761e5379c483a012dd6f3586d63ccc7ff
Joao Pedro <jpsoarxs@gmail.com>
2020-06-01 17:49:35 -0300

    create mongodb/start style

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log("Listagem de usuarios");
-  response.json({
-    'status': true,
-    'message': 'Sua requisição foi atendida com sucesso!'
-  });
-  response.status(200);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/users', (req, res) => {
+  res.json({ status:true });
+  res.status(200);
 });
 
 app.listen(3333);
+console.log("Servidor iniciado com sucesso!");
