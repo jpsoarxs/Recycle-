dc5b007dfdfb784b0f6234ddc9f5a4ba3316fc75
Joao Pedro <jpsoarxs@gmail.com>
2020-06-02 10:59:37 -0300

    first commit

--- server/src/database/index.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-mongoose.connect('mongodb://localhost/azcode', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('MongoDB Connected...'))
-    .catch((err) => console.log(err))
-mongoose.Promise = global.Promise;
-
-export default mongoose;
